19e5e4ba4b892770165d1cb6e323f74f00b6043a
rneisess <ryan.neisess@wsu.edu>
2025-07-26 00:41:23 -0700

    Salve

--- celebrare.js
+++ celebrare.js
@@ -3,44 +3,140 @@
 // Classes, functions, etc.
 
 class Secret {
-    constructor(newAnswer, newHint, status) {
+    constructor(newAnswer, newHints) {
         this.answer = newAnswer; // string
-        this.hint = newHint; // hints can be either strings or Secrets
-        this.solved = status; // bool
+        this.hintsList = newHints; // list of hints
+        this.solved = false; // bool
+        this.unsolvedSecrets = [];
+        this.parentSecret = null;
+        this.parentDiv = null;
+        
+        for (let item of this.hintsList) {
+            if (item instanceof Secret) {
+                this.unsolvedSecrets.push(item);
+                item.parentSecret = this;
+            }
+        }
+    }
+    
+    parseSecrets(hintString) {
+        // To be implemented at a future time
+    }
+
+    // Returns the current corresponding string for the secret
+    inWriting() {
+        if (this.solved) {
+            return this.answer;
+        }
+        else {
+            let displaySecret = "[";
+
+            for (let item of this.hintsList) {
+                if (typeof item == "string") {
+                    displaySecret += item;
+                }
+                else if (item instanceof Secret) { // we *should* have an object at this point...
+                    displaySecret += item.inWriting();
+                }
+                else {
+                    console.log("Error: unexpected data type in hintsList.\n");
+                }
+            }
+
+            return displaySecret + "]";
+        }
     }
 }
+
 
 let user = "Mystery Creature";
-let text1 = "Mow";
+let verified = false;
 
-// function showPuzzle(text) {
-//     // if (user == "Lion") {
-//     //     text = "Mrowl.";
-//     // }
+let lionSecrets = [];
 
-//     document.getElementById("secret1").textContent=text;
-//     document.getElementsByTagName
-// }
+let plantSecret = new Secret(
+    "cattail",
+    [
+        "This fluffy riparian feline ap",
+        new Secret(
+            "end",
+            [
+                "the farthest point from the beginning"
+            ]
+        ),
+        "age sounds like it be",
+        new Secret(
+            "long",
+            [
+                "lomg, tran",
+                new Secret(
+                    "slate",
+                    [
+                        "the finest-grained foliated metamorphic rock, per Wikipedia"
+                    ]
+                ),
+                "d for normies"
+            ]
+        ),
+        "s in Animalia, but can also be found in Plantae."
+    ]
+)
+lionSecrets.push(plantSecret);
 
-function beginPuzzle(secrets) {
-    let lionPuzzles = document.getElementsByClassName("lion puzzle");
-    for (let puzzle of lionPuzzles) {
-        puzzle.style.display = "block";
+let creatureSecrets = [];
+
+// creatureSecrets go here
+
+let unsolvedSecrets = []; // a set of either lion or creature secrets
+let activeSecrets = []; // the set of all active secrets from an entire set
+
+function findTop(currSecret) {
+    if (currSecret.parentSecret == null){
+        return currSecret;
+    }
+    else {
+        return findTop(currSecret.parentSecret);
     }
 }
 
-function verifyUser() {
-    let startText = "Are you the birthday Lion?\n\n\
-        Name this substance to prove your identity."
+function findActiveSecrets(currSecret){
+    if (!currSecret.solved) {
+        if (currSecret.unsolvedSecrets.length > 0) {
+            for (let unsolved of currSecret.unsolvedSecrets) {
+                findActiveSecrets(unsolved);
+            }
+        }
+        else {
+            activeSecrets.push(currSecret);
+        }
+    }
+}
+
+function beginDeciphering(unsolvedSecrets) {
+    for (let majorSecret of unsolvedSecrets) {
+        findActiveSecrets(majorSecret);
+    }
+}
+
+function waitFor(ms) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
+}
+
+function identifyUser() {
+    let startText = "Are you the birthday Lion?\n\nName this substance to prove your identity."
     let solution = "chemistry"
     document.getElementById("start").textContent=startText;
     
-    let verifyResponse = document.getElementById("verifyInput");
+    let textInput = document.getElementById("textInput");
 
-    verifyResponse.addEventListener("keydown", function(e) {
-        if (e.key === "Enter") {
-            if (verifyResponse.value === "Mrowl." || 
-                verifyResponse.value === solution
+    textInput.addEventListener("keydown", async function (e) {
+        if (e.key === "Enter" && !verified) {
+            if (textInput.value == "Mrowl." || 
+                textInput.value == solution
             ) {
                 startText = "Secrets await you, Lion.";
                 user = "Lion";
@@ -50,28 +146,76 @@ function verifyUser() {
             }
             document.getElementById("start").textContent=startText;
             
-            // verifyResponse.value = "";
-            verifyResponse.style.display = "none";
+            textInput.value = "";
+            textInput.style.display = "none";
+            inputBox.style.display = "none";
+
+            await waitFor(2000);
+
+            textInput.style.display = "block";
+            inputBox.style.display = "block";
+            textInput.focus();
+
+            // Input validation complete, setup puzzles & begin
+
+
+            if (user == "Lion") {
+                unsolvedSecrets = lionSecrets;
+
+                let puzzles = document.getElementsByClassName("puzzle");
+                for (let i = 0; i < puzzles.length; i++) {
+                    puzzles[i].style.display = "block";
+                    if (i == 0) {
+                        puzzles[i].textContent = unsolvedSecrets[i].inWriting();
+                        unsolvedSecrets[i].parentDiv = puzzles[i];
+                    }
+                }
+            }
+            else {
+                unsolvedSecrets = creatureSecrets;
+            }
+            
+            verified = true;
+            beginDeciphering(unsolvedSecrets);
+        }
+        else if (e.key === "Enter" && verified) {
+            let guess = textInput.value;
+            // debugger;
+
+            for (let active of activeSecrets) {
+                if (guess == active.answer) {
+                    // mark as solved
+                    active.solved = true;
+
+                    if (active.parentSecret !== null) {
+                        // update parent's list of unsolved secrets
+                        active.parentSecret.unsolvedSecrets =
+                            active.parentSecret.unsolvedSecrets.filter(item => item !== active);
+
+                        // if parent has no more unsolved secrets, make it active
+                        if (active.parentSecret.unsolvedSecrets.length == 0) {
+                            activeSecrets.push(active.parentSecret);
+                        }
+                    }
+
+                    // update text on page
+                    let top = findTop(active);
+                    top.parentDiv.textContent = top.inWriting();
+                    break;
+                }
+            }
+
+            textInput.value = "";
+            textInput.focus();
         }
     })
-    
 }
 
 function incipiatFestum(){
-    let lionSecrets = [];
-    let creatureSecrets = [];
-    let unsolvedSecrets = [];
-    
-    verifyUser();
-    
-    if (user == "Lion") {
-        unsolvedSecrets = lionSecrets;
-    }
-    else {
-        unsolvedSecrets = creatureSecrets;
-    }
+    // Clear forms
+    document.getElementById("inputBox").value = "";
 
-    beginPuzzle(unsolvedSecrets);
+    identifyUser();
 }
 
 // The fun part of the script
